d5c3f92e9ea0c88c08d8b6e8dff94f2f2d3abf4d
salarim <salari.m1375@gmail.com>
2021-04-15 14:57:40 -0700

    Add next button

--- HelloWorldApp/app/src/main/java/org/pytorch/helloworld/MainActivity.java
+++ HelloWorldApp/app/src/main/java/org/pytorch/helloworld/MainActivity.java
@@ -4,7 +4,9 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import org.pytorch.IValue;
 import org.pytorch.Module;
@@ -12,16 +14,28 @@ import org.pytorch.Tensor;
 import org.pytorch.PyTorchAndroid;
 import org.pytorch.torchvision.TensorImageUtils;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 public class MainActivity extends AppCompatActivity {
+  private final String FILE_NAME = "keyFeatures.txt";
   private Module module = null;
   List<float[]> keyFrameFeatures = new ArrayList<float[]>();
   String[] keyFramePaths = null;
+  String[] queryPaths = null;
+  int queryIndex = 0;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -29,23 +43,75 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
     module = PyTorchAndroid.loadModuleFromAsset(getAssets(), "mobile_model.pt");
 
-    Bitmap queryBitmap = null;
-    try {
-      queryBitmap = BitmapFactory.decodeStream(getAssets().open("queries/IMG_0731.JPG"));
-    } catch (IOException e) {
-      Log.e("PytorchHelloWorld", "Error reading assets", e);
-      finish();
-    }
-
-    ((ImageView) findViewById(R.id.image)).setImageBitmap(queryBitmap);
-
     loadKeyFrameFeatures();
     Log.d("output", "keyFrameFeatures length: " + keyFrameFeatures.size());
 
+    Bitmap queryBitmap = loadQuery();
+
+    showClosestKeyFrame(queryBitmap);
+  }
+
+  public float[] runModule(Bitmap bitmap){
+    long startTime = System.nanoTime();
+
+    bitmap = Bitmap.createScaledBitmap(bitmap, 640, 480, false);
+    final Tensor inputTensor = TensorImageUtils.bitmapToFloat32Tensor(bitmap,
+            TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB);
+
+    final Tensor outputTensor = module.forward(IValue.from(inputTensor)).toTensor();
+    float[] feature = outputTensor.getDataAsFloatArray();
+
+    long endTime = System.nanoTime();
+    long duration = (endTime - startTime) / 1000000;
+    Log.d("output", "runModule duration (ms): " + duration);
+    return feature;
+  }
+
+  private void loadKeyFrameFeatures() {
+    try {
+      keyFramePaths = getAssets().list("key_frames");
+      String jsonString = loadFile();
+      if (jsonString.equals("")) {
+        for (String keyFramePath : keyFramePaths) {
+          Bitmap bitmap = BitmapFactory.decodeStream(getAssets().open("key_frames/" + keyFramePath));
+          float[] feature = runModule(bitmap);
+          keyFrameFeatures.add(feature);
+        }
+
+        Gson gson = new Gson();
+        jsonString = gson.toJson(keyFrameFeatures);
+        saveToFile(jsonString);
+      } else {
+        Gson gson = new Gson();
+        Type listOfMyFloats = new TypeToken<ArrayList<float[]>>() {}.getType();
+        keyFrameFeatures = gson.fromJson(jsonString, listOfMyFloats);
+      }
+    } catch (IOException e) {
+      Log.e("PytorchHelloWorld", "Error reading keyframes", e);
+      finish();
+    }
+
+  }
+
+  private Bitmap loadQuery() {
+    Bitmap bitmap = null;
+    try {
+      if (queryPaths == null) queryPaths = getAssets().list("queries");
+
+      bitmap= BitmapFactory.decodeStream(getAssets().open("queries/" + queryPaths[queryIndex]));
+      ((ImageView) findViewById(R.id.image)).setImageBitmap(bitmap);
+    } catch (IOException e) {
+      Log.e("PytorchHelloWorld", "Error reading query", e);
+      finish();
+    }
+
+    return bitmap;
+  }
+
+  private void showClosestKeyFrame(Bitmap queryBitmap) {
     float[] queryFeature = runModule(queryBitmap);
 
     int closestInd = getClosestKeyFrameIndex(queryFeature);
-    Log.d("output", "closest file is: " + keyFramePaths[closestInd]);
 
     Bitmap closestBitmap = null;
     try {
@@ -55,31 +121,13 @@ public class MainActivity extends AppCompatActivity {
       finish();
     }
     ((ImageView) findViewById(R.id.image2)).setImageBitmap(closestBitmap);
-
   }
 
-  public float[] runModule(Bitmap bitmap){
-    bitmap = Bitmap.createScaledBitmap(bitmap, 640, 480, false);
-    final Tensor inputTensor = TensorImageUtils.bitmapToFloat32Tensor(bitmap,
-            TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB);
+  public void nextButtonFunc(View v) {
+    queryIndex = (queryIndex + 1) % queryPaths.length;
 
-    final Tensor outputTensor = module.forward(IValue.from(inputTensor)).toTensor();
-
-    return outputTensor.getDataAsFloatArray();
-  }
-
-  private void loadKeyFrameFeatures() {
-    try {
-      keyFramePaths = getAssets().list("key_frames");
-      for (String keyFramePath: keyFramePaths) {
-        Bitmap bitmap = BitmapFactory.decodeStream(getAssets().open("key_frames/" + keyFramePath));
-        float[] scores = runModule(bitmap);
-        keyFrameFeatures.add(scores);
-      }
-    } catch (IOException e) {
-      Log.e("PytorchHelloWorld", "Error reading keyframes", e);
-      finish();
-    }
+    Bitmap queryBitmap = loadQuery();
+    showClosestKeyFrame(queryBitmap);
   }
 
   private int getClosestKeyFrameIndex(float[] queryFeature) {
@@ -100,5 +148,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
     return min_ind;
+  }
+
+  private void saveToFile(String s) {
+    FileOutputStream fos = null;
+    try {
+      fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+      fos.write(s.getBytes());
+      Toast.makeText(this, "Saved to " + getFilesDir() + "/" + FILE_NAME,
+              Toast.LENGTH_LONG).show();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      if (fos != null) {
+        try {
+          fos.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+  }
+
+  public String loadFile() {
+    FileInputStream fis = null;
+    try {
+      fis = openFileInput(FILE_NAME);
+      InputStreamReader isr = new InputStreamReader(fis);
+      BufferedReader br = new BufferedReader(isr);
+      StringBuilder sb = new StringBuilder();
+      String text;
+      while ((text = br.readLine()) != null) {
+        sb.append(text).append("\n");
+      }
+      Toast.makeText(this, "Loaded from " + getFilesDir() + "/" + FILE_NAME,
+              Toast.LENGTH_LONG).show();
+      return sb.toString();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      if (fis != null) {
+        try {
+          fis.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+    return "";
   }
 }
